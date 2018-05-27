9e8947ec8cea09bc448de242b9513e6e6f9b0bf6
Apache-HB <35050275+Apache-HB@users.noreply.github.com>
2018-05-27 11:11:49 -0400

    created permission data classes and expanded existing classes

--- src/main/kotlin/com/serebit/diskord/data/MessageData.kt
+++ src/main/kotlin/com/serebit/diskord/data/MessageData.kt
@@ -1,3 +1,19 @@
 package com.serebit.diskord.data
 
-data class MessageData(val id: String, val channel_id: String, val content: String)
+import com.serebit.data.UserData
+import com.serebit.data.RoleData
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+data class MessageData(
+    val id: Int,
+    val channel_id: Int,
+    val guild_id: Int,
+    val content: String,
+    val time,
+    val tts: Boolean,
+    val mention_everyone: Boolean,
+    val mentions: Array<UserData>,
+    val mentions_roles: Array<RoleData>
+)
